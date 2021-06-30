c45564a24a50f27675ad0e5331bfb7c7137999b1
FiSSel <golovchenkoartem03@gmail.com>
2021-06-30 16:06:32 +0300

    Добавьте файлы проекта.

---
+++ MyChartsApp/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyChartsApp
+{
+    class Model
+    {
+        public string Year { get; set; }
+        public string BTC { get; set; }
+
+        public Model(string year, string Btc)
+        {
+            Year = year;
+            BTC = Btc;
+        }
+    }
+}
---
+++ MyChartsApp/ViewModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.VisualBasic.FileIO;
+using Microsoft.Win32;
+
+namespace MyChartsApp
+{
+    class ViewModel:ViewModelBase
+    {
+        private string _filePath;
+        private string _name;
+        private TypeUserControl _windowType;
+        private ICommand _switchWindowCommand;
+        private ICommand _openFileCommand;
+        public ObservableCollection<Model> _models;
+        public ICommand SwitchWindowCommand => _switchWindowCommand ?? (_switchWindowCommand = new RelayCommands(arg => SwitchWindow()));
+        public ICommand OpenFileCommand => _openFileCommand ?? (_openFileCommand = new RelayCommands(arg => GetPath()));
+
+        public TypeUserControl WindowType
+        {
+            get { return _windowType; }
+            set
+            {
+                _windowType = value;
+                OnPropertyChanged(nameof(WindowType));
+            }
+        }
+        private void SwitchWindow()
+        {
+            if (WindowType == TypeUserControl.Second)
+            {
+                WindowType = TypeUserControl.First;
+            }
+            else
+            {
+                WindowType = TypeUserControl.Second;
+            }
+        }
+        public string FilePath
+        {
+            get { return _filePath; }
+            set 
+            {
+                _filePath = value;
+                OnPropertyChanged(nameof(FilePath));
+            }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        public ObservableCollection<Model> Models
+        {
+            get { return _models; }
+            set 
+            {
+                _models = value;
+                OnPropertyChanged(nameof(Models));
+            }
+        }
+        private void GetPath()
+        {
+            OpenFileDialog fn = new OpenFileDialog();
+            fn.Filter = "CSV Files (*.csv)|*.csv";
+            if (fn.ShowDialog() == true)
+            {
+                FilePath = fn.FileName;
+                Models= new ObservableCollection<Model>(GetData(FilePath));
+            }
+            else
+            {
+                MessageBox.Show("No chosen file");
+            }
+        }
+        private IEnumerable<Model> GetData(string filePath)
+        {
+            int c = 0;
+            string[] lines = File.ReadAllLines(filePath);
+
+            return lines.Select(line =>
+            {
+                string[] data = line.Split(',');
+                if(c==0)
+                {
+                    Name = Convert.ToString(data[0])+"/"+ Convert.ToString(data[1]);
+            c++;
+                    return new Model(null,null);
+                }
+                else
+                { 
+                return new Model(Convert.ToString(data[0]), Convert.ToString(data[1]));
+                }
+            });
+        }
+    }
+}
