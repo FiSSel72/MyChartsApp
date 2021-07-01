d87726e8d14ab9a9c06c5ad30435c5e20fa93eff
FiSSel <golovchenkoartem03@gmail.com>
2021-07-01 15:30:25 +0300

    UI created

--- MyChartsApp/MainWindow.xaml.cs
+++ MyChartsApp/MainWindow.xaml.cs
@@ -24,5 +24,24 @@ namespace MyChartsApp
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown(-1);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window window = Window.GetWindow(this);
+            window.WindowState = WindowState.Minimized;
+        }
+
+
     }
 }
